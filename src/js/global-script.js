28929710d4a48e24c25edaa63fbe4559d7e891e1
Nikolay Gromov <nicothin@gmail.com>
2018-01-20 01:19:19 +0300

    Не доделана логика при скроле вверх

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -16,20 +16,54 @@ $( document ).ready(function() {
       scrollbars: true,
       mouseWheel: true,
       interactiveScrollbars: true,
+      deceleration: 0.002,
     });
 
     // начнем следить за скроллом
     iScroll[id].on('scrollEnd', function(){
       var scrollY = this.y;
-      var scrollDirection = this.directionY;   console.log('direction: '+scrollDirection);
-      // iScroll[id].scrollToElement(document.querySelector('#'+id+' .page__slide-part:nth-child(2)'));
-      var viewportHeight = $('body').outerHeight();
+      var scrollDirection = this.directionY;
       $('#'+id+' .page__slide-part').each(function(){
-        var innerBlockTop = $(this).position().top; // console.log(innerBlockTop);
-        console.log('позиция верхней границы блока '+$(this).index()+' '+Math.round(innerBlockTop + scrollY));
-        if(Math.round(innerBlockTop + scrollY) <= (viewportHeight - viewportHeight / 3)) console.log('верхняя граница блок '+$(this).index()+' выше низа вьюпорта + 30% высоты');
+        // если скроллим вниз
+        if( scrollDirection === 1 ) {
+          // этот блок виден на странице сейчас, следующий за ним блок существует и тоже виден? УПРТ.
+          if(checkShownSliderPart(this, scrollY) && $(this).next().length && checkShownSliderPart($(this).next(), scrollY)) {
+            var nextBlockTop = Math.round($(this).next().position().top + scrollY);
+            var criticalLine = $('body').outerHeight() - $('body').outerHeight() / 4;
+            // верх следующего блока уже выше нижней четверти высоты вьюпорта?
+            if(nextBlockTop < criticalLine) {
+              // мотаем к следующему
+              var targetPart = $(this).next().index() + 1;
+              iScroll[id].scrollToElement(document.querySelector('#'+id+' .page__slide-part:nth-child('+targetPart+')'), 800);
+            }
+            else {
+              // мотаем так, чтобы низ первого из видимых совпал с низом вьюпорта
+              var targetPosition = Math.round($(this).position().top + $(this).outerHeight() - $('body').outerHeight()) * -1;
+              iScroll[id].scrollTo(0, targetPosition, 800);
+            }
+            return false;
+          }
+        }
+        // если скроллим вверх
+        else {
+          var thisBlockBottom = Math.round($(this).position().top + $(this).outerHeight() + scrollY);
+          var criticalLine = $('body').outerHeight() / 4;
+          console.log(thisBlockBottom+' - '+criticalLine);
+          // нижняя граница первого видимого блока ниже первой четверти высоты вьюпорта, но не ниже нижней границы вьюпорта?
+          // if((thisBlockBottom > criticalLine)) {
+          //   console.log('мотать к верху следующего');
+
+          // }
+          // else {
+          //   // console.log('мотать к низу текущего');
+          //   var targetPart = $(this).next().index() + 1;
+          //   iScroll[id].scrollToElement(document.querySelector('#'+id+' .page__slide-part:nth-child('+targetPart+')'), 800);
+          //   // var targetPosition = Math.round($(this).position().top + $(this).outerHeight() - $('body').outerHeight()) * -1;
+          //   // iScroll[id].scrollTo(0, targetPosition, 800);
+          // }
+          return false;
+        }
       });
-      console.log(' ');
     });
 
     // добавим data-hash атрибуты (для слайдера) на основе id слайда (нужны Swiper-у)
@@ -109,17 +143,37 @@ $( document ).ready(function() {
     $('body').css({backgroundColor: color});
   }
 
-  // ref https://github.com/WICG/EventListenerOptions/pull/30
-  function isPassive() {
-    var supportsPassiveOption = false;
-    try {
-      addEventListener("test", null, Object.defineProperty({}, 'passive', {
-        get: function () {
-          supportsPassiveOption = true;
-        }
-      }));
-    } catch(e) {}
-    return supportsPassiveOption;
+  /**
+   * Проверяет видимость во вьюпорте переданной части слайда
+   * @param  {jquery object} block  проверяемая часть
+   * @param  {int} scroll           скролл на момент проверки
+   * @return {bool}                 истина, если часть сейчас видима на экране
+   */
+  function checkShownSliderPart(block, scroll) {
+    var innerBlockTop = Math.round($(block).position().top);
+    var innerBlockBottom = Math.round($(block).position().top + $(block).outerHeight());
+    var viewportHeight = $('body').outerHeight();
+    if(
+      (innerBlockTop + scroll) <= viewportHeight // верхняя граница блока выше низа вьюпорта
+      &&
+      (innerBlockBottom + scroll) > 0 // нижняя граница блока ниже верха вьюпорта
+    ) {
+      return true;
+    }
+    else return false;
   }
+
+  // ref https://github.com/WICG/EventListenerOptions/pull/30
+  // function isPassive() {
+  //   var supportsPassiveOption = false;
+  //   try {
+  //     addEventListener("test", null, Object.defineProperty({}, 'passive', {
+  //       get: function () {
+  //         supportsPassiveOption = true;
+  //       }
+  //     }));
+  //   } catch(e) {}
+  //   return supportsPassiveOption;
+  // }
 
 });
